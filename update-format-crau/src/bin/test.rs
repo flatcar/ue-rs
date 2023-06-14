1e8e9589792c8c145cb8a95c37ef64b92497e719
William Light <williamlight@microsoft.com>
2023-06-14 21:23:35 +0200

    signature extraction... maybe working?

--- update-format-crau/src/bin/test.rs
+++ update-format-crau/src/bin/test.rs
@@ -1,5 +1,5 @@
+use std::io::{Read, Seek, SeekFrom};
 use std::error::Error;
-use std::io::Read;
 use std::fs;
 
 use protobuf::Message;
@@ -7,38 +7,45 @@ use protobuf::Message;
 use update_format_crau::proto;
 
 
+const DELTA_UPDATE_HEADER_SIZE: u64 = 4 + 8 + 8;
+
 #[derive(Debug)]
 struct DeltaUpdateFileHeader {
     magic: [u8; 4],
-    file_format_version: u64
+    file_format_version: u64,
+    manifest_size: u64
+}
+
+impl DeltaUpdateFileHeader {
+    #[inline]
+    fn translate_offset(&self, offset: u64) -> u64 {
+        DELTA_UPDATE_HEADER_SIZE + self.manifest_size + offset
+    }
 }
 
 fn read_delta_update_header(f: &mut dyn Read) -> Result<DeltaUpdateFileHeader, Box<dyn Error>> {
     let mut header = DeltaUpdateFileHeader {
         magic: [0; 4],
-        file_format_version: 0
+        file_format_version: 0,
+        manifest_size: 0
     };
 
     f.read_exact(&mut header.magic)?;
-
-    let mut buf = [0u8; 8];
-    f.read_exact(&mut buf)?;
-    header.file_format_version = u64::from_be_bytes(buf);
-
-    Ok(header)
-}
-
-fn check_header_invariants(header: &DeltaUpdateFileHeader) -> Result<(), Box<dyn Error>>
-{
     if &header.magic != b"CrAU" {
         return Err("bad file magic".into());
     }
 
+    let mut buf = [0u8; 8];
+    f.read_exact(&mut buf)?;
+    header.file_format_version = u64::from_be_bytes(buf);
     if header.file_format_version != 1 {
         return Err("unsupported file format version".into());
     }
 
-    Ok(())
+    f.read_exact(&mut buf)?;
+    header.manifest_size = u64::from_be_bytes(buf);
+
+    Ok(header)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -46,26 +53,39 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut f = fs::File::open(path)?;
     let header = read_delta_update_header(&mut f)?;
-    check_header_invariants(&header)?;
-
-    let manifest_size = {
-        let mut buf = [0u8; 8];
-        f.read_exact(&mut buf)?;
-        u64::from_be_bytes(buf)
-    };
-
-    println!("{:?}, {}", header, manifest_size);
 
     let manifest_bytes = {
-        let mut buf = vec![0u8; manifest_size as usize];
+        let mut buf = vec![0u8; header.manifest_size as usize];
         f.read_exact(&mut buf)?;
-        buf
+        buf.into_boxed_slice()
     };
 
     let manifest =
         proto::DeltaArchiveManifest::parse_from_bytes(&manifest_bytes)?;
 
-    println!("{:?}", manifest);
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!! signature offsets are from the END of the manifest !!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // this may also be the case for the InstallOperations
+    // use header.translate_offset()
+
+    let signatures_bytes = match (manifest.signatures_offset, manifest.signatures_size) {
+        (Some(sig_offset), Some(sig_size)) => {
+            f.seek(SeekFrom::Start(header.translate_offset(sig_offset)))?;
+
+            let mut buf = vec![0u8; sig_size as usize];
+            f.read_exact(&mut buf)?;
+            Some(buf.into_boxed_slice())
+        },
+        _ => None
+    };
+
+    let signatures = match signatures_bytes {
+        Some(ref bytes) => Some(proto::Signatures::parse_from_bytes(bytes)?),
+        None => None
+    };
+
+    println!("{:?}", signatures);
 
     Ok(())
 }
