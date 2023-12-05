25b85032b5d4e4e57ac19cd744fe3d591ee2c503
Dongsu Park <dpark@linux.microsoft.com>
2023-12-05 15:43:33 +0100

    split hash_on_disk into hash.rs

--- src/download.rs
+++ src/download.rs
@@ -1,8 +1,6 @@
 use anyhow::{Context, Result, bail};
-use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use std::io::Write;
 use std::io;
-use std::fs::File;
-use std::path::Path;
 use log::info;
 use url::Url;
 
@@ -13,48 +11,6 @@ use sha2::{Sha256, Digest};
 pub struct DownloadResult<W: std::io::Write> {
     pub hash: omaha::Hash<omaha::Sha256>,
     pub data: W,
-}
-
-pub fn hash_on_disk_sha256(path: &Path, maxlen: Option<usize>) -> Result<omaha::Hash<omaha::Sha256>> {
-    let file = File::open(path).context(format!("failed to open path({:?})", path.display()))?;
-    let mut hasher = Sha256::new();
-
-    let filelen = file.metadata().context(format!("failed to get metadata of {:?}", path.display()))?.len() as usize;
-
-    let mut maxlen_to_read: usize = match maxlen {
-        Some(len) => {
-            if filelen < len {
-                filelen
-            } else {
-                len
-            }
-        }
-        None => filelen,
-    };
-
-    const CHUNKLEN: usize = 10485760; // 10M
-
-    let mut freader = BufReader::new(file);
-    let mut chunklen: usize;
-
-    freader.seek(SeekFrom::Start(0)).context("failed to seek(0)".to_string())?;
-    while maxlen_to_read > 0 {
-        if maxlen_to_read < CHUNKLEN {
-            chunklen = maxlen_to_read;
-        } else {
-            chunklen = CHUNKLEN;
-        }
-
-        let mut databuf = vec![0u8; chunklen];
-
-        freader.read_exact(&mut databuf).context(format!("failed to read_exact(chunklen {:?})", chunklen))?;
-
-        maxlen_to_read -= chunklen;
-
-        hasher.update(&databuf);
-    }
-
-    Ok(omaha::Hash::from_bytes(hasher.finalize().into()))
 }
 
 pub async fn download_and_hash<U, W>(client: &reqwest::Client, url: U, mut data: W) -> Result<DownloadResult<W>>
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod download;
 pub use download::DownloadResult;
 pub use download::download_and_hash;
-pub use download::hash_on_disk_sha256;
+
+mod hash;
+pub use hash::hash_on_disk_sha256;
 
 pub mod request;
